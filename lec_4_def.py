d42e39d68a7ce2a499fa1571fc000dc737459c4f
MaGGisteRP <72311104+MaGGisteRP@users.noreply.github.com>
2020-11-06 23:30:28 +0200

    Add files via upload

---
+++ lec_4_def.py
@@ -0,0 +1,33 @@
+#функции
+def cyberpunk(coming_december_maybe):
+    _____  |
+    _____  |
+    _____  | - #тело функции 
+    _____  |
+    _____  |
+#--------------------
+def mult_func(a):
+    x = 3*a
+    retune x
+    tmp = mult_func(4)
+print (tmp)
+print (mult_func(10))
+print (mult_func('Good')
+def print_func(a):
+    print(a)
+print('Hello')
+x0 = 10
+def move(t)
+    x = x0*t
+    return x
+print(move(3))
+30
+print(x)
+x = 'Good'
+def my_func():
+    x = 'Bad'
+    print(x)
+nu_func()
+Bad
+print(x)
+Good
