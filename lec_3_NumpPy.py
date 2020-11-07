9fdfd22f6375b071fe0872d03c386a27aa107ff2
MaGGisteRP <72311104+MaGGisteRP@users.noreply.github.com>
2020-11-07 14:38:55 +0200

    Add files via upload

---
+++ lec_3_NumpPy.py
@@ -0,0 +1,6 @@
+import numpy as np
+a = [1, 2, 4]
+b = np.array(a)
+print(type(a))
+print(type(b))
+print (a*a)
---
+++ lec_3_slice.py
@@ -0,0 +1,7 @@
+import numpy as np 
+a = [5, 6, 7, 10, 8, 4, 3]
+print(a[:]) #start = 1 stop = len (a) step = 1
+print(a[3::2])
+print(a[::3])
+A = np.array ([[1,2,3],[4,5,6]])
+print (A[1,:])
